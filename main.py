896928ec87f9440d607e0479edb774ba0006dcd6
Alec Rospierski <asrospie@mtu.edu>
2019-06-07 14:26:42 -0400

    Added spotify api integration

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from lyric_finder import getLyrics
 from lyric_finder import linkBuilder
 import subprocess as sp
+from spotify_info import currently_playing
 
 def main():
     song = ""
     artist = ""
+    username = input("Enter spotify username: ")
     while True:
-        song = input("Enter the Song Title: ")
-        if song.lower() == "exit":
-            print("Goodbye")
-            break
-        artist = input("Enter the Artist's Name: ")
-        if artist.lower() == "exit":
-            print("Goodbye")
-            break
+        song, artist = currently_playing(username)
+        sp.call('clear', shell=True)
+        print("CURRENTLY PLAYING: " + song)
+        print("ARTIST: " + artist)
+        print()
         getLyrics(song, artist)
         print(linkBuilder(song, artist))
         print()
-        input("Press any enter for a new song")
+        response = input("Press any enter for a new song or type exit: ")
+        if response.lower() == "exit":
+            break
         sp.call('clear', shell=True)
 
 main()
